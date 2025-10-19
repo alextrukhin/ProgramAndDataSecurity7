0bfd803389743604aa29eedd83e2014459800d7f
Alex Trukhin <141617261+alextrukhin@users.noreply.github.com>
2025-10-19 18:58:19 +0200

    tweak navigation

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+const labs = [
+	{ name: "Lab 1", path: "/lab1" },
+	{ name: "Lab 2", path: "/lab2" },
+];
+</script>
 <template>
 	<UContainer>
 		<div class="relative flex flex-col text-center py-12">
@@ -6,8 +12,11 @@
 			<p class="text-lg mb-8">
 				This is the main landing page of the application.
 			</p>
-			<UButton to="/lab1" size="lg">Lab 1</UButton>
-			<UButton to="/lab2" size="lg">Lab 2</UButton>
+			<div class="flex flex-col gap-4">
+				<UButton v-for="lab of labs" :to="lab.path" size="lg">
+					{{ lab.name }}
+				</UButton>
+			</div>
 		</div>
 	</UContainer>
 </template>
